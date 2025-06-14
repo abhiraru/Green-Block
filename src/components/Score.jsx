2c14bc8d67c7a7d153829e0f543a9ca869c33682
abhiraru <abishekabiharidad@gmail.com>
2025-06-14 21:40:28 +0530

    css improvements

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -21,7 +21,7 @@ const Score = () => {
       right: 20,
       fontSize: '24px',
       fontWeight: 'bold',
-      color: '#FFFFFF',
+      color: 'green',
     }}>
       Score: {score}
     </div>
